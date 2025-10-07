a7fe38530b2d1c1039dff433dc44836455883dab
Paula Melo <paulamelo2404@gmail.com>
2025-10-07 13:01:37 -0300

    produtos.module configurada

--- src/produtos/produtos.module.ts
+++ src/produtos/produtos.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Produto } from './entities/produtos.entity';
+import { ProdutoService } from './services/produtos.service';
+import { ProdutoController } from './controllers/produdos.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Produto])],
-  providers: [],
-  controllers: [],
+  providers: [ProdutoService],
+  controllers: [ProdutoController],
   exports: [],
 })
 export class ProdutoModule {}
